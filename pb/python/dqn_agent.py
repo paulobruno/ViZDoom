7b33c01425a71acc43e2835661389a55feac7d99
paulobruno <paulobruno@alu.ufc.br>
2017-07-05 15:58:44 -0300

    Put Replay Memory in external class
    Replay Memory as an external class makes it reusable for other scripts.

--- pb/python/dqn_agent.py
+++ pb/python/dqn_agent.py
@@ -98,35 +98,6 @@ def preprocess(image):
     return img
 
 
-class ReplayMemory:
-    def __init__(self, capacity):
-        state_shape = (capacity, game_resolution[0], game_resolution[1], img_channels)
-        self.s1 = np.zeros(state_shape, dtype=np.float32)
-        self.s2 = np.zeros(state_shape, dtype=np.float32)
-        self.a = np.zeros(capacity, dtype=np.int32)
-        self.r = np.zeros(capacity, dtype=np.float32)
-        self.isterminal = np.zeros(capacity, dtype=np.float32)
-
-        self.capacity = capacity
-        self.size = 0
-        self.pos = 0
-
-    def add_transition(self, s1, action, s2, isterminal, reward):
-        self.s1[self.pos, :, :, 0] = s1
-        self.a[self.pos] = action
-        if not isterminal:
-            self.s2[self.pos, :, :, 0] = s2
-        self.isterminal[self.pos] = isterminal
-        self.r[self.pos] = reward
-
-        self.pos = (self.pos+1) % self.capacity
-        self.size = min(self.size + 1, self.capacity)
-
-    def get_sample(self, sample_size):
-        i = sample(range(0, self.size), sample_size)
-        return self.s1[i], self.a[i], self.s2[i], self.isterminal[i], self.r[i]
-
-
 def create_network(session, num_available_actions):
     """ creates the network with 
     conv_relu + max_pool + conv_relu + max_pool + fc + dropout + fc """
@@ -287,7 +258,7 @@ if __name__ == '__main__':
         actions[i, i] = 1
     actions = actions.tolist()
     
-    memory = ReplayMemory(capacity=replay_memory_size)
+    memory = ReplayMemory(capacity=replay_memory_size, game_resolution=game_resolution, img_channels=img_channels)
 
     sess = tf.Session()
     learn, get_q_values, get_best_action = create_network(sess, len(actions))
